efe1459888470866d0b1f146d5d0b9b063d6a66d
Cash Foley <cashfoley@hotmail.com>
2016-03-30 21:26:27 -0500

    Create Initial module

---
+++ Microsoft.SqlDb.DAC.12.psm1
@@ -0,0 +1,11 @@
+﻿
+function Get-SqldbDacFolder
+{
+    Join-Path $PSScriptRoot 'tools'
+}
+
+Export-ModuleMember -Function Get-SqldbDacFolder
+
+$SqlDacDll = Join-Path (Get-SqldbDacFolder) 'Microsoft.SqlServer.Dac.dll'
+add-type -path $SqlDacDll
+
